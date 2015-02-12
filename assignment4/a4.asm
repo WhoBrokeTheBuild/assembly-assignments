51bfc81ebf82c93654e2c327fc8015fee9818b12
Stephen Lane-Walsh <sdl.slane@gmail.com>
2015-02-12 04:56:14 -0500

    'Finished' assignment 4

--- assignment4/a4.asm
+++ assignment4/a4.asm
@@ -1,25 +1,31 @@
 section .data
-
-msg:	db		"%d", 10, 0
-arr:	dw		1, 2, 3, 4, 5, 6
+	msg:	db		"%d", 10, 0
+	arr:	dw		1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	len:	equ 	$-arr
 
 section .bss
 
 section .text
     global main
-	global printf
+	extern printf
 	
 main:
 
+	mov esi, arr ; Beginning of array
+	mov edi, esi ; Copy to edi
+	add edi, len ; End of array
+
+reverse_array:
+	mov ax, word [esi] ; Get the first element we want to switch
+	mov bx, word [edi] ; Get the last element we want to switch
+	mov [edi], ax ; Put the first element in the last spot
+	mov [esi], bx ; Put the last element in the first spot
+	add esi, 2 ; Increment by one word
+	sub edi, 2 ; Decrement by one word
+	cmp esi, edi ; Do they point to the same position?
+	jb reverse_array ; If not, keep going
 	
-	
-	push eax 
-	push msg 
-	call printf ; Call printf with the first arg as msg, and 
-				; the second arg as the result of our calculations
-	add esp, 8
-	
-	mov	ebx,0
-	mov	eax,1
-	int	0x80
+	mov	ebx,0 ; Exit code
+	mov	eax,1 ; Function to call (exit)
+	int	0x80 ; Kernel interrupt, call the exit function
 	
